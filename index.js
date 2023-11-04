c549205916492c8d54b8ddc303927617abd5072f
dominic6809 <muuomusyoki018@gmail.com>
2023-11-04 15:53:27 +0300

    updated file

---
+++ index.js
@@ -0,0 +1,8 @@
+const person = {
+	name: "John Doe",
+	age: 25,
+	nationality: "Kenyan",
+	favourites: ["Apples", "Oranges", "Pizza"],
+	Hobbies: ["Skating", "playing football", "swimming"],
+	profession: "Junior web-developer"
+};
